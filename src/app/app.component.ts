4ea0f55846c15c9c810a8b0f9abcb7a0c7e8a677
josh <joshuatrobertson@protonmail.com>
2021-03-28 17:59:05 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,138 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { CardData } from './card-data.model';
+import { RestartGameComponent } from './restart-game/restart-game.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'card-game-new';
+export class AppComponent implements OnInit {
+
+  cardImages = [
+    'pDGNBK9A0sk',
+    'fYDrhbVlV1E',
+    'qoXgaF27zBc',
+    'b9drVB7xIOI',
+    'TQ-q5WAVHj0'
+  ];
+
+  userFirstTime = 0;
+  userSecondTime = 0;
+
+  // Add a time component
+  time = 0;
+  interval;
+  display;
+
+  cards: CardData[] = [];
+
+  flippedCards: CardData[] = [];
+
+  matchedCount = 0;
+
+
+
+  shuffleArray(anArray: any[]): any[] {
+    return anArray.map(a => [Math.random(), a])
+      .sort((a, b) => a[0] - b[0])
+      .map(a => a[1]);
+  }
+
+
+
+  constructor(private dialog: MatDialog) {
+
+  }
+
+  ngOnInit(): void {
+    this.setupCards();
+  }
+
+  setupCards(): void {
+    this.cards = [];
+    this.cardImages.forEach((image) => {
+      const cardData: CardData = {
+        imageId: image,
+        state: 'default'
+      };
+
+      this.cards.push({ ...cardData });
+      this.cards.push({ ...cardData });
+
+    });
+
+    this.cards = this.shuffleArray(this.cards);
+  }
+
+  cardClicked(index: number): void {
+    const cardInfo = this.cards[index];
+
+    if (cardInfo.state === 'default' && this.flippedCards.length < 2) {
+      cardInfo.state = 'flipped';
+      this.flippedCards.push(cardInfo);
+
+      if (this.flippedCards.length > 1) {
+        this.checkForCardMatch();
+      }
+
+    } else if (cardInfo.state === 'flipped') {
+      cardInfo.state = 'default';
+      this.flippedCards.pop();
+
+    }
+  }
+
+  startTimer() {
+    console.log('=====>');
+    this.interval = setInterval(() => {
+      if (this.time === 0) {
+        this.time++;
+      } else {
+        this.time++;
+      }
+      this.display = this.transform( this.time);
+    }, 1000);
+  }
+  transform(value: number): string {
+    const minutes: number = Math.floor(value / 60);
+    return minutes + ':' + (value - minutes * 60);
+  }
+
+  pauseTimer() {
+    clearInterval(this.interval);
+  }
+
+  checkForCardMatch(): void {
+    setTimeout(() => {
+      const cardOne = this.flippedCards[0];
+      const cardTwo = this.flippedCards[1];
+      const nextState = cardOne.imageId === cardTwo.imageId ? 'matched' : 'default';
+      cardOne.state = cardTwo.state = nextState;
+
+      this.flippedCards = [];
+
+      if (nextState === 'matched') {
+        this.matchedCount++;
+
+        if (this.matchedCount === this.cardImages.length) {
+          clearInterval(this.interval);
+          const dialogRef = this.dialog.open(RestartGameComponent, {
+            disableClose: true
+        });
+          dialogRef.afterClosed().subscribe(() => {
+            this.restart();
+          });
+        }
+      }
+
+    }, 1000);
+  }
+
+  restart(): void {
+    this.matchedCount = 0;
+    this.setupCards();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {RestartGameComponent} from './restart-game/restart-game.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import {GameCardComponent} from './game-card/game-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestartGameComponent,
+    GameCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
