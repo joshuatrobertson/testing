29c94729586bf579e7dbd0e49659332afefdbdc8
josh <joshuatrobertson@protonmail.com>
2021-03-28 18:00:58 +0100

    pushing modified files

---
+++ src/app/restart-game/restart-game.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RestartGameComponent } from './restart-game.component';
+
+describe('RestartGameComponent', () => {
+  let component: RestartGameComponent;
+  let fixture: ComponentFixture<RestartGameComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RestartGameComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RestartGameComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/restart-game/restart-game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-restart-game',
+  templateUrl: './restart-game.component.html',
+  styleUrls: ['./restart-game.component.css']
+})
+export class RestartGameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
